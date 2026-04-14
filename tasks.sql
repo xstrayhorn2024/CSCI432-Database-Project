11a8ec28f0f8a286f0c3fee59b88d2292055fb3d
Xiomara Strayhorn <xiomara.strayhorn@bison.howard.edu>
2026-04-14 16:16:19 -0400

    Add query to retrieve student names for course 101

--- tasks.sql
+++ tasks.sql
@@ -4,3 +4,9 @@ SELECT
     MIN(Score) AS Lowest_Score 
 FROM SUBMISSION 
 WHERE AssignmentID = 501;
+
+SELECT DISTINCT s.FirstName, s.LastName
+FROM STUDENT s
+JOIN ENROLLMENT e ON s.StudentID = e.StudentID
+JOIN COURSE c ON e.CourseID = c.CourseID
+WHERE c.CourseID = 101;
