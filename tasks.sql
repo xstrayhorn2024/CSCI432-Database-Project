5925cbebda3037ed024a237d297afd3695f60beb
Xiomara Strayhorn <xiomara.strayhorn@bison.howard.edu>
2026-04-14 16:16:39 -0400

    Add query to retrieve student assignment scores

--- tasks.sql
+++ tasks.sql
@@ -10,3 +10,15 @@ FROM STUDENT s
 JOIN ENROLLMENT e ON s.StudentID = e.StudentID
 JOIN COURSE c ON e.CourseID = c.CourseID
 WHERE c.CourseID = 101;
+
+SELECT 
+    s.FirstName, 
+    s.LastName, 
+    a.Title AS Assignment_Name, 
+    sub.Score
+FROM STUDENT s
+JOIN ENROLLMENT e ON s.StudentID = e.StudentID
+JOIN SUBMISSION sub ON e.EnrollmentID = sub.EnrollmentID
+JOIN ASSIGNMENT a ON sub.AssignmentID = a.AssignmentID
+WHERE e.CourseID = 101
+ORDER BY s.LastName, a.Title;
