d3d5e183489b7872c65da41a550070be0afda798
Xiomara Strayhorn <xiomara.strayhorn@bison.howard.edu>
2026-04-14 16:13:29 -0400

    Insert sample data for professors and students

--- data.sql
+++ data.sql
@@ -1,1 +1,8 @@
+INSERT INTO PROFESSOR (ProfessorID, FirstName, LastName, Email) VALUES 
+(1, 'Dr. Emmett', 'Brown', 'ebrown@univ.edu'),
+(2, 'Dr. Grace', 'Hopper', 'ghopper@univ.edu');
 
+INSERT INTO STUDENT (StudentID, FirstName, LastName, Email) VALUES 
+(1, 'John', 'Smith', 'jsmith@email.com'),
+(2, 'Alice', 'Quick', 'aquick@email.com'), -- The 'Q' student
+(3, 'Bob', 'Jones', 'bjones@email.com');
