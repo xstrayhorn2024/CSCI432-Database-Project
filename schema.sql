5caf372dc157c76cc204022085a5897fab416eb9
Xiomara Strayhorn <xiomara.strayhorn@bison.howard.edu>
2026-04-14 16:09:38 -0400

    Create PROFESSOR and STUDENT tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
+-- LEVEL 1: Independent Tables
+CREATE TABLE PROFESSOR (
+    ProfessorID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100)
+);
 
+CREATE TABLE STUDENT (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100)
+);
